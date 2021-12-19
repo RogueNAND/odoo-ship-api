0753be1a4fa95b9ac233840b8ed4ae127f3f9720
Christian McClanahan <roguenand@gmail.com>
2021-12-18 21:04:47 -0500

    fix(delivery_api): choose.delivery.carrier wizard returns error if carrier_id is not selected

--- delivery_api/__manifest__.py
+++ delivery_api/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "https://github.com/RogueNAND/odoo-ship-api",
     'category': 'Inventory/Delivery',
     'application': False,
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.0.1',
     'depends': ['delivery', 'product_dimension'],
     'data': [
         'data/ir_cron.xml',
--- delivery_api/models/choose_delivery_carrier.py
+++ delivery_api/models/choose_delivery_carrier.py
@@ -7,9 +7,10 @@ class ChooseDeliveryCarrier(models.TransientModel):
     @api.onchange('carrier_id')
     def _onchange_carrier_id(self):
         self.delivery_message = False
-        vals = self._get_shipment_rate()
-        if vals.get('error_message'):
-            return {'warning': {
-                'title': 'Error',
-                'message': vals['error_message']}
-            }
+        if self.carrier_id:
+            vals = self._get_shipment_rate()
+            if vals.get('error_message'):
+                return {'warning': {
+                    'title': 'Error',
+                    'message': vals['error_message']}
+                }
